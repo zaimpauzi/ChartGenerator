e991f82e66d5315084c34710f03d326370819dc5
Muhammad Zaim <zaimpauzi@outlook.com>
2019-01-22 22:52:35 +0800

    implement method and chart for coral grouping

--- ChartGenerator/MainWindow.xaml.cs
+++ ChartGenerator/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using ChartGenerator.Views;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,34 +25,288 @@ namespace ChartGenerator
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<DataInputSet> InputData = new List<DataInputSet>();
         public MainWindow()
         {
             InitializeComponent();
-            List<DataInputSet> InputData = new List<DataInputSet>();
-            var dataInput1 = new DataInputSet();
-            var dataInput2 = new DataInputSet();
-
-            dataInput1.Width = 50;
-            dataInput1.Height = 10;
-            dataInput1.Depth = 30;
-
-            dataInput2.Width = 100;
-            dataInput2.Height = 80;
-            dataInput2.Depth = 20;
-
-            InputData.Add(dataInput1);
-            InputData.Add(dataInput2);
-            dataGrid.ItemsSource = InputData;
+            var dataInput = new DataInputSet();
+            lengthTextBox.Focus();
+        }
 
 
+        private void SubmitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            BenthicGroup benthicGroup = new BenthicGroup();
+            string itemGroup = benthicGroup.getItemGroup(categoryTextBox.Text);
+            if (itemGroup == null)
+            {
+                MessageBox.Show("Wrong category name. Try again.", "Warning", MessageBoxButton.OK , MessageBoxImage.Warning);
+            }
+
+            else
+            {
+                var dataInput = new DataInputSet();
+                double currentLength = 0;
+                double previousLength = 0;
+
+                try
+                {
+                    currentLength = Convert.ToDouble(lengthTextBox.Text);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Wrong intersect length. Try again.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    goto skip;
+                }
+
+                foreach (var data in InputData)
+                {
+                    previousLength += Convert.ToDouble(data.Length);
+                }
+
+                double deltaLength = currentLength - previousLength;
+                if (deltaLength < 0)
+                {
+                    MessageBox.Show("Intersect length less than previous input. Try again.", "Warning", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    goto skip;
+                }
+
+                dataInput.Length = deltaLength.ToString();
+                dataInput.Depth = depthTextBox.Text;
+                dataInput.Category = categoryTextBox.Text;
+                dataInput.BenthicGroup = itemGroup;
+                InputData.Add(dataInput);
+                dataGrid.ItemsSource = InputData;
+                dataGrid.Items.Refresh();
+
+                lengthTextBox.Text = "";
+                depthTextBox.Text = "";
+                categoryTextBox.Text = "";
+                lengthTextBox.Focus();
+            }
+        skip:;
+
+            }
+
+        private void DataGrid_AutoGeneratingColumn(object sender, DataGridAutoGeneratingColumnEventArgs e)
+        {
+            string displayName = GetPropertyDisplayName(e.PropertyDescriptor);
+            if (!string.IsNullOrEmpty(displayName))
+            {
+                e.Column.Header = displayName;
+            }
+        }
+
+        public static string GetPropertyDisplayName(object descriptor)
+        {
+
+            PropertyDescriptor pd = descriptor as PropertyDescriptor;
+            if (pd != null)
+            {
+                // Check for DisplayName attribute and set the column header accordingly
+                DisplayNameAttribute displayName = pd.Attributes[typeof(DisplayNameAttribute)] as DisplayNameAttribute;
+                if (displayName != null && displayName != DisplayNameAttribute.Default)
+                {
+                    return displayName.DisplayName;
+                }
+
+            }
+            else
+            {
+                PropertyInfo pi = descriptor as PropertyInfo;
+                if (pi != null)
+                {
+                    // Check for DisplayName attribute and set the column header accordingly
+                    Object[] attributes = pi.GetCustomAttributes(typeof(DisplayNameAttribute), true);
+                    for (int i = 0; i < attributes.Length; ++i)
+                    {
+                        DisplayNameAttribute displayName = attributes[i] as DisplayNameAttribute;
+                        if (displayName != null && displayName != DisplayNameAttribute.Default)
+                        {
+                            return displayName.DisplayName;
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+        private void ExportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string CSVFilename = "";
+            List<string> dataList = new List<string>();
+
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.DefaultExt = ".csv";
+            saveDialog.Filter = "CSV documents (.csv)|*.csv";
+            if (saveDialog.ShowDialog() == true)
+            {
+                CSVFilename = saveDialog.FileName;
+            }
+
+            foreach (var data in InputData)
+            {
+                dataList.Add($"{data.Length},{data.Category},{data.BenthicGroup},{data.Depth}");
+            }
+
+            try
+            {
+                File.WriteAllLines(CSVFilename, dataList);
+
+            }
+            catch (Exception)
+            {
+                //
+            }
+        }
+
+        private void ExitItem_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void DataGrid_CurrentCellChanged(object sender, EventArgs e)
+        {
+            foreach (var data in InputData)
+            {
+                string categoryName = data.Category;
+                var benthicGroup = new BenthicGroup();
+                string itemGroup = benthicGroup.getItemGroup(categoryName);
+                data.BenthicGroup = itemGroup;
+            }
+
+            try
+            {
+                dataGrid.Items.Refresh();
+            }
+            catch (Exception)
+            {
+
+                //throw;
+            }
+        }
+
+        private void CreateChartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            
+            ChartsView chartView = new ChartsView();
+            chartView.createPieChart(InputData);
+            chartView.Show();
         }
     }
 
     public class DataInputSet
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
-        public int Depth { get; set; }
+        public string Length { get; set; }
 
+        [DisplayName("Category")]
+        public string Category { get; set; }
+
+        [DisplayName("Benthic Group")]
+        public string BenthicGroup { get; set; }
+
+        public string Depth { get; set; }
+
+    }
+
+    public class BenthicGroup
+    {
+        List<string> Abiotic = new List<string>();
+        List<string> HardCoral = new List<string>();
+        List<string> SoftCoral = new List<string>();
+        List<string> CorallineAlgae = new List<string>();
+        List<string> MacroAlgae = new List<string>();
+        List<string> TurfAlgae = new List<string>();
+        List<string> Sponge = new List<string>();
+        List<string> Other = new List<string>();
+        List<string> Indeterminate = new List<string>();
+        List<string> DeadCoral = new List<string>();
+
+
+        public BenthicGroup()
+        {
+            string[] abiotic = {"RCK", "R", "S" };
+            Abiotic.AddRange(abiotic);
+
+            string[] hardCoral = { "ACX", "ACB", "ACD", "ACE", "ACS", "ACT", "CB", "CE", "CF", "CM", "CS", "CMR", "CL" };
+            HardCoral.AddRange(hardCoral);
+
+            SoftCoral.Add("SC");
+
+            CorallineAlgae.Add("CA");
+
+            string[] macroAlgae = { "HA", "MA", "AO" };
+            MacroAlgae.AddRange(macroAlgae);
+
+            TurfAlgae.Add("TA");
+
+            Sponge.Add("SP");
+
+            string[] other = { "OT", "UNID" };
+            Other.AddRange(other);
+
+            string[] indeterminate = { "IN", "W" };
+            Indeterminate.AddRange(indeterminate);
+
+            DeadCoral.Add("DC");
+
+        }
+
+        public string getItemGroup(string inputItem)
+        {
+            string itemGroup = null;
+
+            if (Abiotic.Contains(inputItem))
+            {
+                itemGroup = "Abiotic";
+            }
+
+            if (HardCoral.Contains(inputItem))
+            {
+                itemGroup = "Hard Coral";
+            }
+
+            if (SoftCoral.Contains(inputItem))
+            {
+                itemGroup = "Soft Coral";
+            }
+
+            if (CorallineAlgae.Contains(inputItem))
+            {
+                itemGroup = "Coralline Algae";
+            }
+
+            if (MacroAlgae.Contains(inputItem))
+            {
+                itemGroup = "Macro Algae";
+            }
+
+            if (TurfAlgae.Contains(inputItem))
+            {
+                itemGroup = "Turf Algae";
+            }
+
+            if (Sponge.Contains(inputItem))
+            {
+                itemGroup = "Sponge";
+            }
+
+            if (Other.Contains(inputItem))
+            {
+                itemGroup = "Other";
+            }
+
+            if (Indeterminate.Contains(inputItem))
+            {
+                itemGroup = "Indeterminate";
+            }
+
+            if (DeadCoral.Contains(inputItem))
+            {
+                itemGroup = "Dead Coral";
+            }
+
+            return itemGroup;
+        }
     }
 }
---
+++ ChartGenerator/Views/ChartsView.xaml.cs
@@ -0,0 +1,237 @@
+﻿using Microsoft.Win32;
+using OxyPlot;
+using OxyPlot.Annotations;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ChartGenerator.Views
+{
+    /// <summary>
+    /// Interaction logic for ChartsView.xaml
+    /// </summary>
+    public partial class ChartsView : Window
+    {
+        private List<DataInputSet> _inputData;
+        public ChartsView()
+        {
+            InitializeComponent();
+
+        }
+
+        public void createPieChart(List<DataInputSet> inputData)
+        {
+            _inputData = inputData;
+            double AbioticCount = 0;
+            double HardCoralCount = 0;
+            double SoftCoralCount = 0;
+            double CorallineAlgaeCount = 0;
+            double MacroAlgaeCount = 0;
+            double TurfAlgaeCount = 0;
+            double SpongeCount = 0;
+            double OtherCount = 0;
+            double IndeterminateCount = 0;
+            double DeadCoralCount = 0;
+
+
+            foreach (var data in inputData)
+            {
+                string benthicGroupName = data.BenthicGroup;
+
+                if (benthicGroupName == "Abiotic")
+                {
+                    AbioticCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Hard Coral")
+                {
+                    HardCoralCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Soft Coral")
+                {
+                    SoftCoralCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Coralline Algae")
+                {
+                    CorallineAlgaeCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Macro Algae")
+                {
+                    MacroAlgaeCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Turf Algae")
+                {
+                    TurfAlgaeCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Sponge")
+                {
+                    SpongeCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Other")
+                {
+                    OtherCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Indeterminate")
+                {
+                    IndeterminateCount += Convert.ToDouble(data.Length);
+                }
+
+                if (benthicGroupName == "Dead Coral")
+                {
+                    DeadCoralCount += Convert.ToDouble(data.Length);
+                }
+            }
+
+            PlotModel pieSeriesView = new PlotModel() { Title = "Benthic Composition" };
+            dynamic pieSeries = new PieSeries { StrokeThickness = 0, InsideLabelPosition = 0.8, AngleSpan = 360, StartAngle = 0 };
+
+            if (AbioticCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Abiotic", AbioticCount) { IsExploded = true });
+            }
+
+            if (HardCoralCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Hard Coral", HardCoralCount) { IsExploded = true });
+            }
+
+            if (SoftCoralCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Soft Coral", SoftCoralCount) { IsExploded = true });
+            }
+
+            if (CorallineAlgaeCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Coralline Algae", CorallineAlgaeCount) { IsExploded = true });
+            }
+
+            if (MacroAlgaeCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Macro Algae", MacroAlgaeCount) { IsExploded = true });
+            }
+
+            if (TurfAlgaeCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Turf Algae", TurfAlgaeCount) { IsExploded = true });
+            }
+
+            if (SpongeCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Sponge", SpongeCount) { IsExploded = true });
+            }
+
+            if (OtherCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Other", OtherCount) { IsExploded = true });
+            }
+
+            if (IndeterminateCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Indeterminate", IndeterminateCount) { IsExploded = true });
+            }
+
+            if (DeadCoralCount != 0)
+            {
+                pieSeries.Slices.Add(new PieSlice("Dead Coral", DeadCoralCount) { IsExploded = true });
+            }
+
+
+            pieSeriesView.Series.Add(pieSeries);
+            chartView.Model = pieSeriesView;
+
+        }
+
+        public void createLineChart(List<DataInputSet> inputData)
+        {
+            double length = 0;
+            List<double> Depth = new List<double>();
+            foreach (var depthData in inputData)
+            {
+                Depth.Add(Convert.ToDouble(depthData.Depth));
+            }
+            double maxDepth = Depth.Max();
+
+            var lineChart = new PlotModel { Title = "Depth Profile"};
+            var series = new LineSeries() { Smooth = true, Color = OxyColors.SandyBrown,};
+
+            foreach (var data in inputData)
+            {
+                string name = data.BenthicGroup;
+                length += Convert.ToDouble(data.Length);
+                double depth = Convert.ToDouble(data.Depth);
+                series.Points.Add(new DataPoint(length, depth));
+                
+            }
+
+            var categoryAxis = new CategoryAxis
+            {
+                Position = AxisPosition.Left,
+                Key = "DepthProfile",
+                StartPosition = 1,
+                EndPosition = 0,
+                Minimum = 0,
+                Maximum = maxDepth + 1,
+                IsAxisVisible = false,
+            };
+
+            
+            lineChart.Series.Add(series);
+            lineChart.Axes.Add(categoryAxis);
+            chartView.Model = lineChart;
+        }
+
+        private void PieChartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            createPieChart(_inputData);
+        }
+
+        private void SeriesBtn_Click(object sender, RoutedEventArgs e)
+        {
+            createLineChart(_inputData);
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string bmpFilePath = "";
+            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap(800, 600, 96, 96, PixelFormats.Pbgra32);
+            renderTargetBitmap.Render(chartView);
+            PngBitmapEncoder bmp = new PngBitmapEncoder();
+            bmp.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+
+            SaveFileDialog saveDialog = new SaveFileDialog();
+            saveDialog.DefaultExt = ".bmp";
+            saveDialog.Filter = "CSV documents (.bmp)|*.bmp";
+            if (saveDialog.ShowDialog() == true)
+            {
+                bmpFilePath = saveDialog.FileName;
+                using (Stream bmpFileStream = File.Create(bmpFilePath))
+                {
+                    bmp.Save(bmpFileStream);
+                }
+            }
+
+            
+        }
+    }
+
+}
